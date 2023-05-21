323eac564d4fbf6e1b578a8401d01827c58766f1
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-20 18:48:04 -0700

    log incase of error starting server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
@@ -10,13 +11,18 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
+
 	srv := &http.Server{
 		Addr: "localhost:8080",
 		Handler: mux,
 		ReadTimeout: 5 * time.Second,
 		WriteTimeout: 3 * time.Second,
 	}
-	srv.ListenAndServe()
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Error starting HTTP server: %v", err)
+	}
 }
 
 
