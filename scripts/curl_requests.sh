e55b1bccd730f5893cb72366ca530030c10e920f
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-23 17:29:37 -0700

    added command to retrieve all users

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -1,3 +1,5 @@
-# hello world
 echo "---- Hitting the 'Hello, World!' endpoint ----"
 curl http://localhost:8080/hello
+
+echo "---- Retrieving all users ----"
+curl http://localhost:8080/api/v1/users
