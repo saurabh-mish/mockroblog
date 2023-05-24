ba97cbd9e0b082ad9154238484d598606c5c7d91
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-23 23:28:20 -0700

    piped output to jq for improved readability

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -2,7 +2,7 @@ echo "---- Hitting the 'Hello, World!' endpoint ----"
 curl http://localhost:8080/hello
 
 echo "---- Retrieving all users ----"
-curl http://localhost:8080/api/v1/users
+curl http://localhost:8080/api/v1/users | jq
 
 echo "---- Retrieving all posts ----"
-curl http://localhost:8080/api/v1/posts
+curl http://localhost:8080/api/v1/posts | jq
