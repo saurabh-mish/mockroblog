c8088a7d6be798790715f788fd435d1fa0fbb429
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-23 23:17:28 -0700

    added curl request to retrieve all posts

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -3,3 +3,6 @@ curl http://localhost:8080/hello
 
 echo "---- Retrieving all users ----"
 curl http://localhost:8080/api/v1/users
+
+echo "---- Retrieving all posts ----"
+curl http://localhost:8080/api/v1/posts
