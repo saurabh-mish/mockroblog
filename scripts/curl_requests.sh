5875e748c5d9def9b50afedc34284838ecdf4ab8
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-31 17:03:18 -0700

    added post request

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -1,8 +1,14 @@
 echo "---- Hitting the 'Hello, World!' endpoint ----"
-curl http://localhost:8080/hello
+curl http://localhost:8080/api/v1/hello
 
 echo "---- Retrieving all users ----"
 curl http://localhost:8080/api/v1/users | jq
 
+echo "---- Retrieving user with ID ----"
+curl http://localhost:8080/api/v1/user/560
+
 echo "---- Retrieving all posts ----"
 curl http://localhost:8080/api/v1/posts | jq
+
+echo "---- Create user ----"
+curl --request POST "http://localhost:8080/api/v1/user?username=testuser&email=testuser@domain.com&password=p@ssword"
