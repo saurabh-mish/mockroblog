b13b33139cbd2680e1936521ffa027309ddc10ba
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-31 17:01:50 -0700

    added new routes

--- pkg/routes/table.go
+++ pkg/routes/table.go
@@ -1,7 +1,19 @@
 package routes
 
+//import "mockroblog/pkg/controllers"
+
 var routes = []route{
-	newRoute("GET", "/hello", Hello),
-	newRoute("GET", "/api/v1/users", AllUsers),
-	newRoute("GET", "/api/v1/posts", AllPosts),
+	newRoute("GET", "/api/v1/hello", Hello),
+
+	newRoute("GET", "/api/v1/users", GetAllUsers),
+	newRoute("POST", "/api/v1/user", CreateUser),
+	newRoute("GET", "/api/v1/user/([0-9]+)", RetrieveUser),
+	// newRoute("PATCH", "/api/v1/user/:id", ModifyUser),
+	// newRoute("DELETE", "/api/v1/user/:id", DeleteUser),
+
+	newRoute("GET", "/api/v1/posts", GetAllPosts),
+	// newRoute("POST", "/api/v1/post", CreatePost),
+	// newRoute("GET", "/api/v1/post/:id", RetrievePost),
+	// newRoute("PATCH", "/api/v1/post/:id", ModifyPost),
+	// newRoute("DELETE", "/api/v1/post/:id", DeletePost),
 }
