894e365f19ccbe9c4b018a6d4acc4b4f345abf98
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-01 10:34:37 -0700

    create user with data instead of query params

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -10,5 +10,10 @@ curl http://localhost:8080/api/v1/user/560
 echo "---- Retrieving all posts ----"
 curl http://localhost:8080/api/v1/posts | jq
 
+#echo "---- Create user ----"
+#curl --request POST "http://localhost:8080/api/v1/user?username=testuser&email=testuser@domain.com&password=p@ssword"
+
 echo "---- Create user ----"
-curl --request POST "http://localhost:8080/api/v1/user?username=testuser&email=testuser@domain.com&password=p@ssword"
+curl --request POST --url http://localhost:8080/api/v1/user \
+	--header "Content-Type: application/json" \
+	--data '{"username":"testuser","email":"testuser@domain.com","password":"p@ssword"}'
