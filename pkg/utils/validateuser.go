1c443e42931422b82a83df213ac57ee4bc4af9ca
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-01 12:08:16 -0700

    check validation error and output as http response

--- pkg/utils/validateuser.go
+++ pkg/utils/validateuser.go
@@ -1,17 +1,19 @@
 package utils
 
+
 import (
 	"strings"
+	"errors"
 )
 
-func ValidateCreateUser(username, password, email string) bool {
+func ValidateCreateUser(username, password, email string) (bool, error) {
 	if len(username) < 5 || len(username) > 20 {
-		return false
+		return false, errors.New("Length of username incorrect")
 	} else if len(password) < 6 || !strings.ContainsAny(password, "@!$#&*") {
-		return false
-	} else if !strings.ContainsAny(email, "@") || !strings.ContainsAny(email, ".com") {
-		return false
+		return false, errors.New("Password too short or doesn't contain special characters")
+	} else if !strings.Contains(email, ".com") || !strings.Contains(email, "@") {
+		return false, errors.New("Invalid email address")
 	} else {
-		return true
+		return true, nil
 	}
 }
