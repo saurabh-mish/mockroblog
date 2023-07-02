a7e10db6c9730083859f46fb9da165c59159cbcb
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-02 13:53:21 -0700

    added new routes for post and vote microservices

--- pkg/routes/table.go
+++ pkg/routes/table.go
@@ -5,15 +5,20 @@ package routes
 var routes = []route{
 	newRoute("GET", "/api/v1/hello", Hello),
 
+	// account microservice
 	newRoute("GET", "/api/v1/users", GetAllUsers),
 	newRoute("POST", "/api/v1/user", CreateUser),
 	newRoute("GET", "/api/v1/user/([0-9]+)", RetrieveUser),
-	// newRoute("PATCH", "/api/v1/user/:id", ModifyUser),
-	// newRoute("DELETE", "/api/v1/user/:id", DeleteUser),
 
-	newRoute("GET", "/api/v1/posts", GetAllPosts),
-	// newRoute("POST", "/api/v1/post", CreatePost),
-	// newRoute("GET", "/api/v1/post/:id", RetrievePost),
-	// newRoute("PATCH", "/api/v1/post/:id", ModifyPost),
-	// newRoute("DELETE", "/api/v1/post/:id", DeletePost),
+	// post microservice
+	newRoute("POST",   "/api/v1/post", CreatePost),
+	newRoute("DELETE", "/api/v1/post/id=([0-9]+)", DeletePost),
+	newRoute("GET",    "/api/v1/post/id=([0-9]+)", RetrievePost),
+	newRoute("GET", "/api/v1/posts/number=([0-9]+)", RetrieveRecentPosts),
+	newRoute("GET", "/api/v1/posts/number=([0-9]+)&community=([^/]+)", RetrieveRecentPostsFromCommunity),
+
+	// vote microservice
+	newRoute("POST", "/api/v1/vote/post/id=([0-9]+)&action=([^/]+)", CastVoteForPost),
+	newRoute("GET", "/api/v1/votes/post/id=([0-9]+)", GetVotesForPost),
+	newRoute("GET", "/api/v1/votes/number=([0-9]+)", GetTopScoringPosts),
 }
