70f5f2ea38e88391ee7bc0ae25b287eb34b0a393
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-01 20:49:00 -0700

    removed unused imports

--- pkg/routes/posthandler.go
+++ pkg/routes/posthandler.go
@@ -3,18 +3,16 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
-	"fmt"
-	"strconv"
 
 	"mockroblog/pkg/models"
 )
 
-func GetAllPosts(w http.ResponseWriter, r *http.Request) {
-	allPosts := models.Posts{
-		{nil, "title 1", "filler content for post 1", "community 1"},
-		{nil, "title 2", "filler content for post 2", "community 2"},
-	}
+var allPosts = models.Posts{
+	{nil, "title 1", "filler content for post 1", "community 1"},
+	{nil, "title 2", "filler content for post 2", "community 2"},
+}
 
+func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	allPostsJSON, _ := json.Marshal(allPosts)
 
 	w.Header().Set("Content-Type", "application/json")
@@ -22,6 +20,5 @@ func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte(allPostsJSON))
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
 	}
 }
