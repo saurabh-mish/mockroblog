871c608d22e8e9b070962f3006e072044d1525cb
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-01 20:45:19 -0700

    retrieve and create user linked with slice of user struct

--- pkg/routes/userhandler.go
+++ pkg/routes/userhandler.go
@@ -5,26 +5,23 @@ import (
 	"net/http"
 	"strconv"
 	"fmt"
-	//"strings"
-	//"io"
 
 	"mockroblog/pkg/models"
 	"mockroblog/pkg/utils"
 )
 
+var allUsersJSON = []models.User{
+	{12, "user1", "user1@domain.com", "", 2},
+	{23, "user2", "user2@domain.com", "", 20},
+	{11, "user3", "user3@domain.com", "", 8},
+}
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	allUsersJSON := []models.User{
-		{12, "user1", "user1@domain.com", "", 2},
-		{23, "user2", "user2@domain.com", "", 20},
-		{11, "user3", "user3@domain.com", "", 8},
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	err := json.NewEncoder(w).Encode(&allUsersJSON)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
 	}
 }
 
@@ -58,47 +55,56 @@ func GetAllUsers(w http.ResponseWriter, r *http.Request) {
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Content-Type") != "application/json" {
 		http.Error(w, "Request not acceptable; check header", http.StatusNotAcceptable)
-		return
 	}
 
 	var userData models.User
 	err := json.NewDecoder(r.Body).Decode(&userData)
-
 	if err != nil {
 		http.Error(w, "Could not parse user payload", http.StatusUnprocessableEntity)
-		return
 	}
 
 	_, err = utils.ValidateCreateUser(userData.Username, userData.Password, userData.Email)
 
 	if err != nil {
 		http.Error(w, fmt.Sprint(err), http.StatusUnprocessableEntity)
-		return
 	} else {
+		//fmt.Fprintf(w, "User created:\n%v", userData)
+		allUsersJSON = append(allUsersJSON, userData)
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
 		_, err := w.Write([]byte("Welcome " + userData.Username + "!\nWe have sent a confirmation email to " + userData.Email + "\n"))
 		if err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
 		}
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	//w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "User created:\n%v", userData)
 }
 
 
 func RetrieveUser(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.ParseUint(getField(r, 0), 10, 16)
-	if err != nil {
-		http.Error(w, "Unable to convert ID to int", http.StatusUnprocessableEntity)
-		return
+	userId, _ := strconv.ParseUint(getField(r, 0), 10, 16)
+	// regexp will take integers from 0 to 9; below code unreachable
+	// if err != nil {
+	// 	http.Error(w, "Unable to convert ID to int", http.StatusUnprocessableEntity)
+	// }
+
+	var found bool = false
+
+	for _, user := range allUsersJSON {
+		if uint64(user.Id) == userId {
+			found = true
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(w).Encode(user)
+			if err != nil {
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+			}
+		}
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "The value of ID is %v\n", id)
+	if found == false && userId >= 0 {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
