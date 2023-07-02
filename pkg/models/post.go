7e1fd277782f9816c2a1916f0671ec2d9a05d972
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-02 13:54:19 -0700

    ID not nil; TODO handle when integrating with DB

--- pkg/models/post.go
+++ pkg/models/post.go
@@ -1,7 +1,7 @@
 package models
 
 type Post struct {
-	Id *int `json:"-"`
+	Id int `json:"id"`
 	Title string `json:"title"`
 	PostContent string `json:"post_content"`
 	Community string `json:"community"`
