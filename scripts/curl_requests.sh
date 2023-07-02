52d784e9f2690c79fd6a8ef4424f09b0b241a880
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-01 20:43:21 -0700

    commented before microservice sections

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -1,19 +1,28 @@
-echo "---- Hitting the 'Hello, World!' endpoint ----"
+echo "---- 'Hello, World!' endpoint ----"
 curl http://localhost:8080/api/v1/hello
 
-echo "---- Retrieving all users ----"
+##############################
+#### Account Microservice ####
+##############################
+
+echo "---- Retrieve all users ----"
 curl http://localhost:8080/api/v1/users | jq
 
-echo "---- Retrieving user with ID ----"
+echo "---- Retrieve non-existing user ----"
 curl http://localhost:8080/api/v1/user/560
-
-echo "---- Retrieving all posts ----"
-curl http://localhost:8080/api/v1/posts | jq
-
-#echo "---- Create user ----"
-#curl --request POST "http://localhost:8080/api/v1/user?username=testuser&email=testuser@domain.com&password=p@ssword"
 
 echo "---- Create user ----"
 curl --request POST --url http://localhost:8080/api/v1/user \
 	--header "Content-Type: application/json" \
 	--data '{"username":"testuser","email":"testuser@domain.com","password":"p@ssword"}'
+
+echo "---- Retrieve existing user with ID ----"
+curl http://localhost:8080/api/v1/user/0 | jq
+
+###########################
+#### Post Microservice ####
+###########################
+
+echo "---- Retrieve all posts ----"
+curl http://localhost:8080/api/v1/posts | jq
+
