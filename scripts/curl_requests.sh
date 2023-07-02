5912b9cbe7701fa242c77560474304e81a728589
Saurabh Mishra <saurabh.mish@icloud.com>
2023-07-02 13:54:51 -0700

    http requests for post and vote microservices

--- scripts/curl_requests.sh
+++ scripts/curl_requests.sh
@@ -1,3 +1,4 @@
+# Getting started
 echo "---- 'Hello, World!' endpoint ----"
 curl http://localhost:8080/api/v1/hello
 
@@ -8,10 +9,7 @@ curl http://localhost:8080/api/v1/hello
 echo "---- Retrieve all users ----"
 curl http://localhost:8080/api/v1/users | jq
 
-echo "---- Retrieve non-existing user ----"
-curl http://localhost:8080/api/v1/user/560
-
-echo "---- Create user ----"
+echo "---- Create new user ----"
 curl --request POST --url http://localhost:8080/api/v1/user \
 	--header "Content-Type: application/json" \
 	--data '{"username":"testuser","email":"testuser@domain.com","password":"p@ssword"}'
@@ -23,6 +21,19 @@ curl http://localhost:8080/api/v1/user/0 | jq
 #### Post Microservice ####
 ###########################
 
-echo "---- Retrieve all posts ----"
-curl http://localhost:8080/api/v1/posts | jq
+echo "---- Create new post ----"
+curl --request POST --url http://localhost:8080/api/v1/post \
+	--header "Content-Type: application/json" \
+	--data '{"title":"title 1","content":"The quick brown fox jumps over the lazy dog","community":"playground"}'
 
+echo "---- Retrieve existing post with ID ----"
+curl http://localhost:8080/api/v1/post/id=14 | jq
+
+echo "---- Delete existing post ----"
+curl -X DELETE http://localhost:8080/api/v1/post/id=3
+
+echo "---- Retrieve n most recent posts ----"
+curl http://localhost:8080/api/v1/posts/number=1
+
+echo "---- Retrieve n most recent posts from community ----"
+curl "http://localhost:8080/api/v1/posts/number=1&community=playground"
